1c8b84002d640cdc80fc8378c10a6c1d7abc1c01
Haoyu Li <hli73@ur.rochester.edu>
2018-12-29 01:28:14 -0500

    Adding some functionalities st. it persists to local storage

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 //Load all event listeners
 function loadEventListeners(){
+    //DOM Load Event
+    document.addEventListener('DOMContentLoaded',getTasks);
     //Add task event
     form.addEventListener('submit',addTask);
     //Remove task event 
@@ -19,6 +21,35 @@ function loadEventListeners(){
     //Filter tasks Event
     filter.addEventListener('keyup',filterTasks);
 };
+
+//Get Tasks from LS
+function getTasks(){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+       tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task){
+    //Create li element
+    const li = document.createElement('li');
+    //Add class
+    li.className = 'collection-item';
+    //Create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    //Create new link element
+    const link = document.createElement('a');
+    //Add class
+    link.className = 'delete-item secondary-content';
+    //Add icon html
+    link.innerHTML = '<i class="far fa-trash-alt"></i>';
+    //Append the link to li
+    li.appendChild(link);
+
+    //Append li to ul
+    taskList.appendChild(li);
+    });
+}
 
 
 //Add Task
@@ -45,8 +76,25 @@ function addTask(e){
     //Append li to ul
     taskList.appendChild(li);
 
+
+    //Store in local storage
+    storeTaskInLocalStorage(taskInput.value);
+
     //Clear Input
     taskInput.value = '';
+    
+
+ //Store Task
+ function storeTaskInLocalStorage(task){
+     let tasks;
+     if(localStorage.getItem('tasks') === null){
+        tasks = [];
+     }else{
+         tasks = JSON.parse(localStorage.getItem('tasks'));
+     }
+     tasks.push(task);
+     localStorage.setItem('tasks',JSON.stringify(tasks));
+ }
     
 
 
@@ -59,9 +107,32 @@ function removeTask(e){
     //The parent is of the icon is the a tag, the parent of the a tag is the li
     if(confirm('Are You Sure?')){
     e.target.parentElement.parentElement.remove();
+
+    //Remove from local storage
+    removeTaskFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
 }
+
+
+//Remove from local storage
+function removeTaskFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+       tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task,index){
+        if(taskItem.textContent === task){
+            tasks.splice(index,1);
+        }
+    });
+
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+
 
 //Clear Tasks
 function clearTasks(){
@@ -71,7 +142,16 @@ function clearTasks(){
     while(taskList.firstChild){
         taskList.removeChild(taskList.firstChild);
     }
+
+    //Clear from Local Storage
+    clearTasksFromLocalStorage();
 }
+
+//Clear tasks from Local Storage
+function clearTasksFromLocalStorage(){
+    localStorage.clear();
+}
+
 
 //Filter Tasks
 function filterTasks(e){
